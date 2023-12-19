d80e872ab5c75beb2177ee0d13300f2e0b1c0d9d
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2023-12-19 00:26:38 -0500

    added AddPet and have a placeholder component

---
+++ front/src/components/AddPet/AddPet.js
@@ -0,0 +1,12 @@
+//temporary implementation
+import React from 'react';
+
+const AddPet = () => {
+  return (
+    <div>
+      <h1>Add a New Pet</h1>
+    </div>
+  );
+};
+
+export default AddPet;
